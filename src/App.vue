fc4c8dcbea0664f43762db158d7bd132cd3b473b
bigcookie <5748299+biggestcookie@users.noreply.github.com>
2022-03-27 00:48:41 -0400

    Initial commit adding vue

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const content = ["Hello", "world"];
+</script>
+
+<template>
+  <div>
+    <table>
+      <tr v-for="word in content" v-bind:key="word">
+        <td>{{ word }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    proxy: {
+      "/api": "http://localhost:8080",
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        charset: false,
+      },
+    },
+  },
+});
